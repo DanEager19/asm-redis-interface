ede3ab0347941df079390527e08adb306775e45e
Daniel Eager <daniel@deager.dev>
2022-04-25 16:21:16 -0500

    Started lol

--- redis-interface.asm
+++ redis-interface.asm
@@ -5,7 +5,9 @@ segment .data
 	get	db	"Enter the key:",10,0
 	set1	db	"Enter the key:",10,0
 	set2	db	"Enter the value:",10,0
-
+	input1	db	"/usr/bin/users",0
+	output1	db	"--help",0
+	out2	dd	output1,0
 segment .bss
 
 
@@ -16,8 +18,11 @@ asm_main:
 	push	ebp
 	mov	ebp, esp
 	; ********** CODE STARTS HERE **********
-
-	
+	mov	eax, 11
+	mov	ebx, input1
+	lea	ecx, DWORD[out2]
+	mov	edx, 0
+	int	0x80
 
 	; *********** CODE ENDS HERE ***********
 	mov	eax, 0
@@ -29,9 +34,9 @@ setter:
 	push	ebp
 	mov	ebp, esp
 	
-	mov	eax, 11
-	mov	ebx, 1
-	lea	ecx, someStr
+;	mov	eax, 11
+;	mov	ebx, 1
+;	lea	ecx, someStr
 	
 
 	mov	esp, ebp
