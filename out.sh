7bb5d5d09b655c26996c11652db9b73f23db85e1
Daniel Eager <daniel@deager.dev>
2022-04-29 22:34:00 -0500

    Getter works hooray

--- out.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo hi $1
--- redis-interface.asm
+++ redis-interface.asm
@@ -2,49 +2,99 @@
 
 
 segment .data
-	get	db	"Enter the key:",10,0
-	set1	db	"Enter the key:",10,0
-	set2	db	"Enter the value:",10,0
-	input1	db	"/usr/bin/users",0
-	output1	db	"--help",0
-	out2	dd	output1,0
-segment .bss
+	scanf_fmt	db	"%s",0
+	printf_key	db	"Enter the key: ",0
+	printf_value	db	"Enter the value: ",0
+	
+	redis	db	"/usr/bin/redis-cli",0
+	rset	db	"set",0
+	rget	db	"get",0
 
+	argv_get	dd	redis,rget,key,0
+	argv_set	dd	redis,rset,key,value,0
+
+segment .bss
+	key	db	100
+	value	db	100
 
 segment .text
 	global  asm_main
+	extern	printf
+	extern	scanf
 
 asm_main:
 	push	ebp
 	mov	ebp, esp
 	; ********** CODE STARTS HERE **********
-	mov	eax, 11
-	mov	ebx, input1
-	lea	ecx, DWORD[out2]
-	mov	edx, 0
-	int	0x80
+
+	call	setter
 
 	; *********** CODE ENDS HERE ***********
 	mov	eax, 0
 	mov	esp, ebp
 	pop	ebp
 	ret
-
-setter: 
+getter:
 	push	ebp
 	mov	ebp, esp
-	
-;	mov	eax, 11
-;	mov	ebx, 1
-;	lea	ecx, someStr
-	
+
+	mov	esi, printf_key
+	push	esi
+	call	printf
+	add	esp, 4
+
+	mov	edi, scanf_fmt
+	lea	esi, BYTE[key]
+	push	esi
+	push	edi
+	call	scanf
+	add	esp, 8
+
+	mov	eax, 11
+	mov	ebx, redis
+	lea	ecx, argv_get
+	mov	edx, 0
+	int	0x80
 
 	mov	esp, ebp
 	pop	ebp
 	ret
-getter:
+
+setter:
 	push	ebp
 	mov	ebp, esp
+
+	mov	esi, printf_key
+	push	esi
+	call	printf
+	add	esp, 4
+
+	mov	edi, scanf_fmt
+	lea	esi, BYTE[key]
+	push	esi
+	push	edi
+	call	scanf
+	pop	edi
+	pop	esi
+	add	esp, 8
+
+	mov	esi, printf_value
+	push	esi
+	call	printf
+	add	esp, 4
+
+	mov	edi, scanf_fmt
+	lea	esi, BYTE[value]
+	push	esi
+	push	edi
+	call	scanf
+	add	esp, 8
+
+	mov	eax, 11
+	mov	ebx, redis
+	lea	ecx, argv_set
+	mov	edx, 0
+	int	0x80
 
 	mov	esp, ebp
 	pop	ebp
