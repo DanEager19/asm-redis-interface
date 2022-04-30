8adc5a682213e84c29faf0c96399ba714ff32232
Daniel Eager <daniel@deager.dev>
2022-04-30 14:13:14 -0500

    Everything works?

--- redis-interface.asm
+++ redis-interface.asm
@@ -1,7 +1,8 @@
 %include "/usr/local/share/csc314/asm_io.inc"
 
-
 segment .data
+	interface	db	"1) Set Value",10,"2) Get Value",10,"3) Delete All",10,"0) Quit",10,0
+	
 	scanf_fmt	db	"%s",0
 	printf_key	db	"Enter the key: ",0
 	printf_value	db	"Enter the value: ",0
@@ -14,8 +15,8 @@ segment .data
 	argv_set	dd	redis,rset,key,value,0
 
 segment .bss
-	key	db	100
-	value	db	100
+	key	resb	100
+	value	resb	100
 
 segment .text
 	global  asm_main
@@ -27,34 +28,70 @@ asm_main:
 	mov	ebp, esp
 	; ********** CODE STARTS HERE **********
 
-	call	setter
+	do:
+	mov	eax, interface
+	call	print_string
+	call	read_int
 
+	cmp	eax, 0
+	je	done
+	cmp	eax, 1
+	je	call_set
+	cmp	eax, 2
+	je	call_get
+	jmp	do
+
+	call_set:
+	call	setter
+	jmp	do
+
+	call_get:
+	call	getter
+	jmp	do
+
+	done:
+	
 	; *********** CODE ENDS HERE ***********
 	mov	eax, 0
 	mov	esp, ebp
 	pop	ebp
 	ret
-getter:
+
+set_key:
 	push	ebp
 	mov	ebp, esp
 
-	mov	esi, printf_key
-	push	esi
+	mov	eax, printf_key
+	push	eax
 	call	printf
 	add	esp, 4
 
-	mov	edi, scanf_fmt
-	lea	esi, BYTE[key]
-	push	esi
-	push	edi
+	mov	ebx, scanf_fmt
+	lea	eax, BYTE[key]
+	push	eax
+	push	ebx
 	call	scanf
 	add	esp, 8
 
-	mov	eax, 11
-	mov	ebx, redis
-	lea	ecx, argv_get
-	mov	edx, 0
-	int	0x80
+	mov	esp, ebp
+	pop	ebp
+	ret
+
+set_value:
+	push	ebp
+	mov	ebp, esp
+
+	mov	eax, printf_value
+	push	eax
+	call	printf
+	add	esp, 4
+
+	mov	ebx, scanf_fmt
+	lea	eax, BYTE[value]
+	push	eax
+	push	ebx
+	call	scanf
+	add	esp, 8
 
 	mov	esp, ebp
 	pop	ebp
@@ -63,37 +100,30 @@ getter:
 setter:
 	push	ebp
 	mov	ebp, esp
-
-	mov	esi, printf_key
-	push	esi
-	call	printf
-	add	esp, 4
-
-	mov	edi, scanf_fmt
-	lea	esi, BYTE[key]
-	push	esi
-	push	edi
-	call	scanf
-	pop	edi
-	pop	esi
-	add	esp, 8
-
-	mov	esi, printf_value
-	push	esi
-	call	printf
-	add	esp, 4
-
-	mov	edi, scanf_fmt
-	lea	esi, BYTE[value]
-	push	esi
-	push	edi
-	call	scanf
-	add	esp, 8
+	
+	call	set_key
+	call	set_value
 
 	mov	eax, 11
 	mov	ebx, redis
-	lea	ecx, argv_set
-	mov	edx, 0
+	lea	ecx, [argv_set]
+	xor	edx, edx
+	int	0x80
+
+	mov	esp, ebp
+	pop	ebp
+	ret
+
+getter:
+	push	ebp
+	mov	ebp, esp
+
+	call	set_key
+
+	mov	eax, 11
+	mov	ebx, redis
+	lea	ecx, [argv_get]
+	xor	edx, edx
 	int	0x80
 
 	mov	esp, ebp
